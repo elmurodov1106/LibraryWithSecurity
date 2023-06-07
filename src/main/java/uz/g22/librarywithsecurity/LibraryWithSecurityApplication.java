7a4b24a62ce9577882ba7cd638162a5af89dd910
Msi Laptop <hh@gg.com>
2023-06-07 14:30:30 +0500

    Initial commit

---
+++ src/main/java/uz/g22/librarywithsecurity/LibraryWithSecurityApplication.java
@@ -0,0 +1,13 @@
+package uz.g22.librarywithsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWithSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryWithSecurityApplication.class, args);
+    }
+
+}
